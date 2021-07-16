de7c41c92d5f7b3b87e271747e97c505951b6907
Nobuyori Takahashi <nobuyoritakahashi-air@Nobuyoris-MacBook-Air.local>
2021-07-16 19:08:23 +0900

    Corrected WebSocket template code and parameter name build_token changed to builder_token

--- src/servers/ws/main.go
+++ src/servers/ws/main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/Diarkis/diarkis/log"
 	"github.com/Diarkis/diarkis/mesh"
 	"github.com/Diarkis/diarkis/server"
-	"github.com/Diarkis/dairkis/ws"
-	"io/ioutil"
 	"os"
 )
 
@@ -22,22 +20,5 @@ func main() {
 	server.SetupAsWebSocketServer(fmt.Sprintf("%s/configs/ws/main.json", rootpath))
 	cmds.Setup(rootpath)
 	cmds.ExposeServer()
-	// this assumes there is nginx running along side and bin/tools/findport outputs /tmp/NGINX_PORT
-	diarkis.OnReady(func(next func(error)) {
-		bytes, err := ioutil.ReadFile("/tmp/NGINX_PORT")
-		if err != nil {
-			// we failed to read nginx port, we ignore it
-			next(nil)
-			return
-		}
-		nginxport := string(bytes)
-		switch os.Getenv("DIARKIS_CLOUD_ENV") {
-			case "AZURE_WS":
-				pubEP, _ := util.GetPublicEndPointMSLB()
-				ws.SetPublicEndPointWithPort(pubEP, nginxport)
-			// TODO: add GCP and AWS version here too
-		}
-		next(nil)
-	})
 	diarkis.Start()
 }
