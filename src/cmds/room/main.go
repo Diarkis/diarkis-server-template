b38f02e44c5b6fae6ec80e480db32fc4cb77fcb0
Nobuyori Takahashi <nobuyoritakahashi-air@Nobuyoris-MacBook-Air.local>
2021-09-26 15:00:09 +0900

    Added logger to room command

--- src/cmds/room/main.go
+++ src/cmds/room/main.go
@@ -1,17 +1,22 @@
 package roomcmds
 
 import (
+	"github.com/Diarkis/diarkis/log"
 	"github.com/Diarkis/diarkis/room"
 	"github.com/Diarkis/diarkis/roomSupport"
 )
+
+var logger = log.New("room")
 
 func Expose() {
 	room.ExposeCommands()
 	roomSupport.ExposeCommands()
 	// we broadcast user ID to room members when the client leaves unexpectedly
+	// The message will be propagated by OnMemberLeave even on the client
 	room.SetOnDiscardCustomMessage(onDiscardCustomMessage)
 }
 
 func onDiscardCustomMessage(roomID string, userID string) []byte {
+	logger.Debug("OnDiscardCustomMessage roomID:%v userID:%v", roomID, userID)
 	return []byte(userID)
 }
