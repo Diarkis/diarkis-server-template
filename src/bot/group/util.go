434d13b13fb7bcc761107e736697601ae5739ba4
kairi <kairi@diarkis.io>
2022-01-14 15:43:55 +0900

    add group broadcast bot

---
+++ src/bot/group/util.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// returns addr, sid, key, iv, mackey, error
+func auth(uid int) (string, []byte, []byte, []byte, []byte, error) {
+	url := fmt.Sprintf("http://%s/auth/%v", host, uid)
+	// fmt.Printf("Bot ID: %v - Connecting to HTTP: %s\n", uid, url)
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	//req.Header.Add("ClientKey", clientKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	data := make(map[string]interface{})
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	if _, ok := data["sid"]; !ok {
+		return "", nil, nil, nil, nil, err
+	}
+	sid, err := hex.DecodeString(data["sid"].(string))
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	//uuid, _ := uuidv4.FromBytes(sid)
+	//fmt.Println("sid:", sid)
+	//fmt.Println("uuid:", uuid.String)
+	encKey, err := hex.DecodeString(data["encryptionKey"].(string))
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	encIV, err := hex.DecodeString(data["encryptionIV"].(string))
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	encMacKey, err := hex.DecodeString(data["encryptionMacKey"].(string))
+	if err != nil {
+		return "", nil, nil, nil, nil, err
+	}
+	addr := ""
+	if _, ok := data[strings.ToUpper(proto)]; ok {
+		addr = data[strings.ToUpper(proto)].(string)
+	}
+	return addr, sid, encKey, encIV, encMacKey, nil
+}
+func parseArgs() {
+	host = os.Args[1]
+	botsSource, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("How many bot parameter given is invalid %v\n", err)
+		os.Exit(1)
+		return
+	}
+	bots = botsSource
+
+	packetSource, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("Packet size parameter given is invalid %v\n", err)
+		os.Exit(1)
+		return
+	}
+	packetSize = packetSource
+
+	intervalSource, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Printf("Interval of broadcast parameter given is invalid %v\n", err)
+		os.Exit(1)
+		return
+	}
+	interval = int64(intervalSource)
+}
