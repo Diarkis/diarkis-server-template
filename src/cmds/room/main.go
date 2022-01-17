5a6c095bd102631f0be40577cdb6d2f25038075f
Nobuyori Takahashi <nt@diarkis.io>
2022-01-17 17:40:09 +0900

    Room added server push on owner change

--- src/cmds/room/main.go
+++ src/cmds/room/main.go
@@ -6,8 +6,12 @@ import (
 	"github.com/Diarkis/diarkis/log"
 	"github.com/Diarkis/diarkis/room"
 	"github.com/Diarkis/diarkis/roomSupport"
+	"github.com/Diarkis/diarkis/user"
 	"strconv"
 )
+
+const ver uint8 = 3
+const onRoomOwnerChangeCmd uint16 = 100
 
 var logger = log.New("room")
 
@@ -17,6 +21,10 @@ func Expose() {
 	// we broadcast user ID to room members when the client leaves unexpectedly
 	// The message will be propagated by OnMemberLeave even on the client
 	room.SetOnDiscardCustomMessage(onDiscardCustomMessage)
+	// Sends a custom broadcast to all members when the room owner changes
+	room.SetOnRoomOwnerChange(onRoomOwnerChange)
+	// When a user joins a room, the user receives the room owner ID by message
+	room.AfterJoinRoomCmd(afterJoinRoom)
 	// set up custom mesh commands
 	meshCmds.Setup()
 }
@@ -32,4 +40,33 @@ func onDiscardCustomMessage(roomID string, userID string, sid string) []byte {
 	binary.LittleEndian.PutUint64(bytes, uint64(conv))
 	logger.Debug("OnDiscardCustomMessage message user ID %v bytes %v", conv, bytes)
 	return bytes
+}
+
+func onRoomOwnerChange(params interface{}) {
+	data := params.(map[string]string)
+	roomID := data["roomID"]
+	ownerID := data["ownerID"]
+	syncRoomOwnerID(roomID, ownerID)
+}
+
+func afterJoinRoom(ver uint8, cmd uint16, payload []byte, userData *user.User, next func(error)) {
+	roomID := room.GetRoomID(userData)
+	if roomID == "" {
+		// user is not in the room
+		next(nil)
+		return
+	}
+	ownerID := room.GetRoomOwnerID(roomID)
+	if ownerID == "" {
+		// there is no owner yet...
+		next(nil)
+		return
+	}
+	syncRoomOwnerID(roomID, ownerID)
+}
+
+func syncRoomOwnerID(roomID string, ownerID string) {
+	logger.Debug("OnRoomOwnerChange => broadcast the new owner ID %s to room %s", ownerID, roomID)
+	message := []byte(ownerID)
+	room.Announce(roomID, room.GetMemberIDs(roomID), ver, onRoomOwnerChangeCmd, message, true)
 }
