6fad51b1dc55816c172b1351c4bb8c3437bb20d6
Diarkis Test Client <dtc@dtc.local>
2022-08-31 12:06:51 +0900

    WebSocket MatchMaker fixed

--- src/ws_cmds/matchmaker/main.go
+++ src/ws_cmds/matchmaker/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/Diarkis/diarkis/room"
 )
 
+const sampleTicketType uint8 = 0
+
 var logger = log.New("matching")
 
 func Expose(rootpath string) {
@@ -15,7 +17,7 @@ func Expose(rootpath string) {
 	matching.Setup(fmt.Sprintf("%s/configs/shared/matching.json", rootpath))
 
 	// Set up matching ticket
-	matching.SetOnIssueTicket(func(userData *user.User) *matching.TicketParams {
+	matching.SetOnIssueTicket(sampleTicketType, func(userData *user.User) *matching.TicketParams {
 		return &matching.TicketParams{
 			ProfileIDs:     []string{"RankMatch", "RankMatch2"},
 			MaxMembers:     2,
@@ -28,11 +30,13 @@ func Expose(rootpath string) {
 			Properties: map[string]int{"rank": 1},
 		}
 	})
-	matching.SetOnTicketMatch(func(userData *user.User, roomID string, memberIDs []string) bool {
+	matching.SetOnTicketMatch(sampleTicketType,
+		func(t *matching.Ticket, owner, userData *user.User, roomID string, memberIDs []string) bool {
+
 		// add custom logic to decide matchmaking completion here
 		return false
 	})
-	matching.SetOnTicketComplete(func(roomID string, userData *user.User) []byte {
+	matching.SetOnTicketComplete(sampleTicketType, func(roomID string, userData *user.User) []byte {
 		memberIDs := room.GetMemberIDs(roomID)
 		return []byte(fmt.Sprintf("Ticket matchmaking complete => %v", memberIDs))
 	})
