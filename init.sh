342db408cb840f1d389ae195443288f7fa5b8b9c
kairi <kairi@diarkis.io>
2024-03-29 18:57:00 +0900

    add init.sh and update Makefile

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function usage {
+    cat <<EOF
+$(basename ${0}) is a generate diarkis project tool.
+
+Usage:
+    $(basename ${0}) moduleName builderToken outputPath
+Sample:
+    $(basename ${0}) github.com/sample-origanization/sample-project sampleToken /tmp/sample-project
+
+EOF
+    exit 1
+}
+
+if [ $# -ne 3 ]; then
+    usage
+fi
+
+project_id=$1
+builder_token=$2
+output_path=$3
+
+go run ./tools/install.go $project_id $builder_token $output_path
+cd $output_path &&
+    go mod edit -module $project_id &&
+    find . -type f -name '*.go'  -exec sed -i '' -e "s%github.com/Diarkis/diarkis-server-template%$project_id%g" {} \; &&
+    sed -i '' -e "s%github.com/Diarkis/diarkis-server-template%$project_id%g" puffer/gen.sh
+
