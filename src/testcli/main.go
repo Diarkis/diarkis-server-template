66591ce9e8470c3ede8393f798b47210eba01b1a
Kohji Nakahata <koji.nakahata@diarkis.io>
2024-06-27 21:19:16 +0900

    feat: added Go test client.

---
+++ src/testcli/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/Diarkis/diarkis-server-template/testcli/resonance"
+	"github.com/Diarkis/diarkis/client/go/test/cli"
+)
+
+var (
+	tcpResonance *resonance.Resonance
+	udpResonance *resonance.Resonance
+)
+
+func main() {
+	cli.SetupBuiltInCommands()
+
+	// You can add custom commands to the CLI.
+	cli.RegisterCommands("test", []cli.Command{
+		{CmdName: "resonate", Desc: "Resonate your message", CmdFunc: resonate},
+	})
+
+	cli.Connect()
+
+	// setup custom module
+	if cli.TCPClient != nil {
+		tcpResonance = resonance.SetupAsTCP(cli.TCPClient)
+	}
+	if cli.UDPClient != nil {
+		udpResonance = resonance.SetupAsUDP(cli.UDPClient)
+	}
+
+	cli.Run()
+}
+
+func resonate() {
+	// This is a sample command to add test commands to the CLI.
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Which client to join a room? [tcp/udp]")
+	client, _ := reader.ReadString('\n')
+
+	fmt.Println("Enter the message you want to resonate.")
+	message, _ := reader.ReadString('\n')
+
+	switch client {
+	case "tcp\n":
+		if tcpResonance == nil {
+			return
+		}
+		tcpResonance.Resonate(message)
+	case "udp\n":
+		if udpResonance == nil {
+			return
+		}
+		udpResonance.Resonate(message)
+	}
+}
