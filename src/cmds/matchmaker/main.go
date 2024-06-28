160ab45199c3e230264b8050545e0fac35891ab8
kairi <kairi@diarkis.io>
2024-06-28 18:21:31 +0900

    add ticketType1

--- src/cmds/matchmaker/main.go
+++ src/cmds/matchmaker/main.go
@@ -8,14 +8,15 @@ import (
 	"github.com/Diarkis/diarkis/util"
 )
 
-const sampleTicketType uint8 = 0
+const sampleTicketType0 uint8 = 0 // max member 2
+const sampleTicketType1 uint8 = 1 // max member 4
 
 var logger = log.New("matching")
 
 func Setup() {
 	// Set up matching ticket with sampleTicketType
 	// This callback is invoked when a new ticket is issued.
-	matching.SetOnIssueTicket(sampleTicketType, func(userData *user.User) *matching.TicketParams {
+	matching.SetOnIssueTicket(sampleTicketType0, func(userData *user.User) *matching.TicketParams {
 		return &matching.TicketParams{
 			ProfileIDs:     []string{"RankMatch"},
 			MaxMembers:     2,
@@ -36,7 +37,7 @@ func Setup() {
 	// This callback is invoked when a ticket finds a match.
 	// By returning true, the callback allows the found match to be matched.
 	// By returning false, the callback rejects the found match and ignores it.
-	matching.SetOnTicketMatch(sampleTicketType,
+	matching.SetOnTicketMatch(sampleTicketType0,
 		func(t *matching.Ticket, owner, userData *user.User, roomID string, memberIDs []string) bool {
 
 			// add custom logic to decide matchmaking completion here
@@ -45,8 +46,50 @@ func Setup() {
 
 	// Set up a callback on ticket complete with sampleTicketType
 	// This callback is invoked when a ticket completes the matchmaking.
-	matching.SetOnTicketComplete(sampleTicketType, func(ticketProps *matching.TicketProperties, owner *user.User) []byte {
-		memberIDs, _ := matching.GetTicketMemberIDs(sampleTicketType, owner)
+	matching.SetOnTicketComplete(sampleTicketType0, func(ticketProps *matching.TicketProperties, owner *user.User) []byte {
+		memberIDs, _ := matching.GetTicketMemberIDs(sampleTicketType0, owner)
+
+		list := make([]string, len(memberIDs)+1)
+
+		// the first element is the owner user ID
+		list[0] = owner.ID
+
+		index := 1
+
+		for i := 0; i < len(memberIDs); i++ {
+			list[index] = memberIDs[i]
+			index++
+		}
+
+		return packet.StringListToBytes(list)
+	})
+
+	matching.SetOnIssueTicket(sampleTicketType1, func(userData *user.User) *matching.TicketParams {
+		return &matching.TicketParams{
+			ProfileIDs:     []string{"RankMatch"},
+			MaxMembers:     4,
+			SearchInterval: 100, // 100ms
+			SearchTries:    uint8(util.RandomInt(0, 300)),
+			EmptySearches:  3,
+			TicketDuration: 60, // 1m
+			HowMany:        20,
+			// Change here as you see fit according to your application needs
+			Tag: "",
+			// Change here as you see fit according to your application needs
+			AddProperties:    map[string]int{"rank": 1},
+			SearchProperties: map[string][]int{"rank": {1, 2, 3, 4, 5}},
+		}
+	})
+	matching.SetOnTicketMatch(sampleTicketType1,
+		func(t *matching.Ticket, owner, userData *user.User, roomID string, memberIDs []string) bool {
+			logger.Debugf("sampleTicketType1 is matched!", "owner", owner, "userData.ID", userData.ID, "memberIDs", memberIDs)
+			// add custom logic to decide matchmaking completion here
+			return false
+		})
+
+	matching.SetOnTicketComplete(sampleTicketType1, func(ticketProps *matching.TicketProperties, owner *user.User) []byte {
+		logger.Debug("SetOnTickerComplete")
+		memberIDs, _ := matching.GetTicketMemberIDs(sampleTicketType1, owner)
 
 		list := make([]string, len(memberIDs)+1)
 
