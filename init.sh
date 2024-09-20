58fd6a46a62f408dbfcc6315590e24e919e176f7
Henry Obein <henry@diarkis.io>
2024-09-20 17:53:58 +0900

    init.sh: test module name validity using go mod init

--- init.sh
+++ init.sh
@@ -15,6 +15,8 @@ EOF
     exit 1
 }
 
+CURR_PWD=`pwd`
+
 # Accept optional module name
 if [ $# -ne 3 -a $# -ne 4 ]; then
     usage
@@ -30,6 +32,33 @@ output_path=$3
 if [ -z "${module_name}" ]; then
     module_name=$(basename $output_path)
 fi
+
+# check module name using go mod init
+tmp_dir=
+
+cleanup() {
+    cd ${CURR_PWD}
+    if [ ! -z "$tmp_dir" ]; then
+        rm -fr "$tmp_dir"
+    fi
+}
+
+trap cleanup EXIT
+
+tmp_dir=`mktemp -d -q`
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    echo "fail to create temporary directory"
+    exit 1
+fi
+
+pushd ${tmp_dir}
+    go mod init ${module_name}
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+popd
+rm -fr "$tmp_dir"
 
 go run ./tools/install.go $project_id $builder_token $output_path
 go run ./tools/rewrite_import.go $output_path "github.com/Diarkis/diarkis-server-template" "$module_name"
