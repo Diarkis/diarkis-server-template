2b5d1235ccaa50f0a718eeb27d6dcd4c13259d40
Henry Obein <henry@diarkis.io>
2024-10-15 16:30:59 +0900

    tools: add go script to add example to existing template

---
+++ tools/add_example/go.mod
@@ -0,0 +1,12 @@
+module add_example
+
+go 1.21.12
+
+require (
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/goccy/go-yaml v1.12.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ tools/add_example/main.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+)
+
+func main() {
+	var listExample, addExample bool
+	var destDir string
+
+	flag.BoolVar(&listExample, "list", false, "List available example.")
+	flag.BoolVar(&addExample, "add", false, "Add one example.")
+	flag.StringVar(&destDir, "destDir", "", "Where to add the example")
+
+	flag.Parse()
+
+	if listExample {
+		l, err := getExampleList()
+		if err != nil {
+			fmt.Printf("fail to retrieve the example list. %v\n", err)
+			os.Exit(1)
+		}
+		sort.Strings(l)
+		for _, name := range l {
+			fmt.Printf("- %s\n", name)
+		}
+		return
+	}
+
+	for _, arg := range flag.Args() {
+		ok, err := validateExample(arg)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("ok: %t\n", ok)
+
+		err = copyExampleToTargetTemplate(destDir, "examples/"+arg, arg)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func validateExample(name string) (bool, error) {
+	examples, err := getExampleList()
+	if err != nil {
+		return false, err
+	}
+
+	for _, ex := range examples {
+		if ex == name {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func getExampleList() ([]string, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	examplesDir := path.Join(cwd, "examples")
+	stat, err := os.Stat(examplesDir)
+	if err != nil {
+		return nil, err
+	}
+	if !stat.IsDir() {
+		return nil, errors.New("examples is expected to be a folder")
+	}
+
+	var examples []string
+	err = filepath.Walk(examplesDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+		if info.Name() == "go.mod" {
+			// found one example
+			rel, err := filepath.Rel(examplesDir, filepath.Dir(path))
+			if err != nil {
+				return err
+			}
+			examples = append(examples, filepath.ToSlash(rel))
+		}
+		return nil
+	})
+
+	return examples, err
+}
+
+// getTargetTemplateDiarkisBuildInfo retrieve the diarkis.project_id
+// and builder_token from an existing template.
+func getTargetTemplateDiarkisBuildInfo(templateDir string) (string, string, error) {
+	linuxBuildFile := filepath.Join(templateDir, "build", "linux-build.yml")
+	linuxData, err := os.ReadFile(linuxBuildFile)
+	if err != nil {
+		return "", "", err
+	}
+
+	var o struct {
+		Diarkis struct {
+			ProjectID    string `yaml:"project_id"`
+			BuilderToken string `yaml:"builder_token"`
+		} `yaml:"diarkis"`
+	}
+	err = yaml.Unmarshal(linuxData, &o)
+	if err != nil {
+		return "", "", err
+	}
+
+	return o.Diarkis.ProjectID, o.Diarkis.BuilderToken, nil
+}
+
+func copyExampleToTargetTemplate(templateDir, exampleDir, exampleName string) error {
+	projectID, builderToken, err := getTargetTemplateDiarkisBuildInfo(templateDir)
+	if err != nil {
+		return err
+	}
+
+	exampleOutDir := filepath.Join(templateDir, "examples", exampleName)
+	err = os.MkdirAll(exampleOutDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	var yamlFiles []string
+	err = filepath.Walk(exampleDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		// compute relative path from exampleDir
+		rel, err := filepath.Rel(exampleDir, path)
+		if err != nil {
+			return err
+		}
+
+		if strings.EqualFold(filepath.Ext(info.Name()), ".yml") {
+			yamlFiles = append(yamlFiles, rel)
+		}
+
+		destPath := filepath.Join(exampleOutDir, rel)
+		if info.IsDir() {
+			err = os.MkdirAll(destPath, info.Mode().Perm())
+			return err
+		}
+
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		err = func() error {
+			srcFile, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer srcFile.Close()
+			dstFile, err := os.OpenFile(destPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, info.Mode().Perm())
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(dstFile, srcFile)
+			if err != nil {
+				// best effort
+				_ = dstFile.Close()
+				_ = os.Remove(destPath)
+				return err
+			}
+
+			return nil
+		}()
+
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// post process yaml file
+	for _, rel := range yamlFiles {
+		destPath := filepath.Join(exampleOutDir, rel)
+		err = updateBuildSettings(destPath, projectID, builderToken)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func updateBuildSettings(filename, projectID, builderToken string) error {
+	stat, err := os.Stat(filename)
+	if err != nil {
+		return err
+	}
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	var m map[string]any
+	err = yaml.Unmarshal(data, &m)
+	if err != nil {
+		// not the same format
+		return nil
+	}
+
+	if _, ok := m["buildSettings"]; !ok {
+		// not the same format
+		return nil
+	}
+
+	diarkis, ok := m["diarkis"]
+	if !ok {
+		// not the same format
+		return nil
+	}
+
+	diarkisMap, ok := diarkis.(map[string]any)
+	if !ok {
+		// not the same format
+		return nil
+	}
+
+	for _, k := range []string{"project_id", "builder_token"} {
+		v, ok := diarkisMap[k]
+		if !ok {
+			return nil
+		}
+		_, ok = v.(string)
+		if !ok {
+			return nil
+		}
+	}
+
+	diarkisMap["project_id"] = projectID
+	diarkisMap["builder_token"] = builderToken
+
+	updated, err := yaml.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, updated, stat.Mode().Perm())
+}
