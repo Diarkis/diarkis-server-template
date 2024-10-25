347e934325dfa31492a1b0584d47f059bb644209
Henry Obein <henry@diarkis.io>
2024-10-25 10:57:01 +0900

    root magefile: add example list/add

--- magefiles/magefiles.go
+++ magefiles/magefiles.go
@@ -6,8 +6,11 @@
 package main
 
 import (
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
+
+type Examples mg.Namespace
 
 // Init Initialize a new project from the template, parameters: project_id builder_token output module_name
 //
@@ -15,4 +18,14 @@ import (
 // init 012345678 sampleToken /tmp/sample-project github.com/sample-organization/sample-project
 func Init(projectID, builderToken, output, moduleName string) error {
 	return sh.RunV("go", "run", "./tools/init.go", projectID, builderToken, output, moduleName)
+}
+
+// List list available example
+func (Examples) List() error {
+	return sh.RunV("go", "run", "./tools/add_example.go", "-list")
+}
+
+// Add add a specific sample to an existing project
+func (Examples) Add(name, projectDir string) error {
+	return sh.RunV("go", "run", "./tools/add_example.go", "-add", "-destDir", projectDir, name)
 }
