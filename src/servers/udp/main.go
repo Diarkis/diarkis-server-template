6e3318241de958f40a82e8c3b7fe3d9e48ba484e
Henry Obein <henry@diarkis.io>
2025-02-13 12:46:18 +0900

    remove starting / from config path

--- src/servers/udp/main.go
+++ src/servers/udp/main.go
@@ -8,23 +8,23 @@ import (
 )
 
 func main() {
-	logConfigPath := "/configs/shared/log.json"
-	meshConfigPath := "/configs/shared/mesh.json"
+	logConfigPath := "configs/shared/log.json"
+	meshConfigPath := "configs/shared/mesh.json"
 
 	diarkisexec.SetupDiarkis(logConfigPath, meshConfigPath, &diarkisexec.Modules{
-		Room:       &diarkisexec.Options{ConfigPath: "/configs/shared/room.json", ExposeCommands: true},
+		Room:       &diarkisexec.Options{ConfigPath: "configs/shared/room.json", ExposeCommands: true},
 		P2P:        &diarkisexec.Options{ExposeCommands: true},
-		Group:      &diarkisexec.Options{ConfigPath: "/configs/shared/group.json", ExposeCommands: true},
-		Dive:       &diarkisexec.Options{ConfigPath: "/configs/shared/dive.json", ExposeCommands: true},
-		Field:      &diarkisexec.Options{ConfigPath: "/configs/shared/field.json", ExposeCommands: true},
-		DM:         &diarkisexec.Options{ConfigPath: "/configs/shared/dm.json", ExposeCommands: true},
-		MatchMaker: &diarkisexec.Options{ConfigPath: "/configs/shared/matching.json", ExposeCommands: true},
-		Session:    &diarkisexec.Options{ConfigPath: "/configs/shared/session.json", ExposeCommands: true},
+		Group:      &diarkisexec.Options{ConfigPath: "configs/shared/group.json", ExposeCommands: true},
+		Dive:       &diarkisexec.Options{ConfigPath: "configs/shared/dive.json", ExposeCommands: true},
+		Field:      &diarkisexec.Options{ConfigPath: "configs/shared/field.json", ExposeCommands: true},
+		DM:         &diarkisexec.Options{ConfigPath: "configs/shared/dm.json", ExposeCommands: true},
+		MatchMaker: &diarkisexec.Options{ConfigPath: "configs/shared/matching.json", ExposeCommands: true},
+		Session:    &diarkisexec.Options{ConfigPath: "configs/shared/session.json", ExposeCommands: true},
 	})
 
 	cmds.SetupUDP()
 
-	diarkisexec.SetupDiarkisUDPServer("/configs/udp/main.json")
+	diarkisexec.SetupDiarkisUDPServer("configs/udp/main.json")
 
 	diarkisexec.StartDiarkis()
 }
