31e0f32038922fb283a09526686f3d3153feace5
kasader <casada980@gmail.com>
2025-07-03 15:55:52 +0900

    Clean up comments

--- examples/matching/custom-criteria/servers/udp/main.go
+++ examples/matching/custom-criteria/servers/udp/main.go
@@ -17,10 +17,12 @@ import (
 
 var logger = log.New("UDP")
 
-const ticketDuration = 15 // 15 seconds
-// maxAllowedDistance Maximum distance allowed for two users to match together.
-const maxAllowedDistance = 5000
-const ticketType = uint8(1)
+const (
+	// maxAllowedDistance is the maximum distance allowed for two users to match together.
+	maxAllowedDistance = 5000
+	ticketType         = uint8(1)
+	ticketDuration     = 15 // 15 seconds
+)
 
 func main() {
 	logConfigPath := "configs/shared/log.json"
@@ -46,11 +48,11 @@ func setupMaching() {
 
 		candidateProps, _ := ticketProps.GetCandidateByUID(candidate.ID)
 
-		// Retrieve the application data from the ticket props.
+		// Retrieve the application data from the ticket properties.
 		binary.Read(bytes.NewReader(ticketProps.GetOwner().ApplicationData), binary.BigEndian, &ownerCoords)
 		binary.Read(bytes.NewReader(candidateProps.ApplicationData), binary.BigEndian, &candidateCoords)
 
-		// Here compute the distance between the owner and candidate.
+		// Here we compute the distance between the owner and candidate.
 		distance := common.ComputeDistanceHubeny(ownerCoords, candidateCoords)
 		if distance > maxAllowedDistance {
 			logger.Debugf("Cannot match candidates", "Owner", owner.ID, "Candidate", candidate.ID,
@@ -96,22 +98,26 @@ func handleStartMatching(ver uint8, cmd uint16, payload []byte, userData *user.U
 		return
 	}
 
-	var latitude, longitude float64
+	var coords *common.Coordinates
 	if false {
-		// Here you are supposed to retrieve the client address.
+		// As stated by the note in the README.md, if you were to implement a geolocation database,
+		// you could attempt to retrieve the coordinates of the client by their IP address.
+		//
+		// We do not provide this, but we opt to leave it as a functional stub to encourage creative
+		// use of Diarkis Matchmaker.
 		userAddr := userData.UDPState.GetClientAddr()
-		latitude, longitude, _ = geolocalizeAddress(userAddr)
+		latitude, longitude, _ := geolocalizeAddress(userAddr)
+		coords = &common.Coordinates{Latitude: latitude, Longitude: longitude}
 	} else {
-		// As an example we use the client data as coordinates.
-		latitude = params.Latitude
-		longitude = params.Longitude
+		// As an example we use the client data as coordinates:
+		coords = &common.Coordinates{Latitude: params.Latitude, Longitude: params.Longitude}
 	}
 
 	// Pack the user coordinates as an application data.
 	// This is a lazy way to pack the user coordinates in a byte array.
 	// We recommend to use something else in production.
 	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.BigEndian, []float64{latitude, longitude})
+	binary.Write(buf, binary.BigEndian, coords)
 
 	// Randomize searchTries and emptySearches to not move to the wait mode at the same time considering all clients issue tickets at the same time.
 	searchTries := randomInt(1, 10)
@@ -124,7 +130,7 @@ func handleStartMatching(ver uint8, cmd uint16, payload []byte, userData *user.U
 		EmptySearches:  uint8(emptySearches),
 		TicketDuration: ticketDuration,
 		HowMany:        20,
-		// Change here as you see fit according to your application needs
+		// Change here as you see fit according to your application needs.
 		Tags: nil,
 		// The profile we use has no criteria because we focus on implementing
 		// a custom check.
@@ -154,7 +160,7 @@ func handleStartMatching(ver uint8, cmd uint16, payload []byte, userData *user.U
 }
 
 func geolocalizeAddress(address string) (latitude float64, longitude float64, err error) {
-	// Here you are supposed to lookup the address in a geo DB.
+	// Here you may look up the address in a geolocation database.
 	_ = address
 	latitude = 35.65877910898138
 	longitude = 139.70128360250285
