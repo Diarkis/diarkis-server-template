96fa6d63eff0dd8a84a39639ad3a1d3c2af628ee
kairi <kairi@diarkis.io>
2025-11-07 16:54:53 +0900

    add fakesignal utility

---
+++ src/fakesignal/README.md
@@ -0,0 +1,35 @@
+# fakesignal
+
+Unix signal (SIGUSR1, SIGUSR2, SIGHUP) emulation utility for Windows.
+
+## Overview
+
+Since Windows does not have Unix signals like SIGUSR1 or SIGUSR2, this utility emulates these signals using Windows Event API. It is used to send signals to Diarkis server processes.
+
+## Features
+
+- **Supported signals**: SIGUSR1, SIGUSR2, SIGHUP
+- Signal emulation using Windows Event API
+- Send signals by specifying process ID (PID)
+
+## Usage
+
+```bash
+fakesignal <PID> <SIGNAL>
+```
+
+### Examples
+```bash
+fakesignal 1234 SIGUSR1
+fakesignal 5678 SIGHUP
+```
+
+## Implementation Details
+
+- `main.go` - CLI tool for sending signals
+- `signal.go` - Signal handling mechanism (used in-process)
+- Windows Event name format: `DIARKIS#SIGNAL_<SIGNAME>#PID_<PID>`
+
+## Build Requirements
+
+`//go:build windows` - Windows platform only
---
+++ src/fakesignal/main.go
@@ -0,0 +1,68 @@
+//go:build windows
+
+// This is a utility to send a signal (SIGUSR1, SIGUSR2 or SIGHUP)
+// to diarkis process.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"syscall"
+
+	"golang.org/x/sys/windows"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("usage: %s <PID> <SIGNAL>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	pidStr := os.Args[1]
+	signalStr := os.Args[2]
+
+	pid, err := strconv.Atoi(pidStr)
+	if err != nil {
+		fmt.Printf("invalid pid %q\n", pidStr)
+		os.Exit(1)
+	}
+
+	var sig syscall.Signal
+	switch signalStr {
+	case "SIGUSR1":
+		sig = SIGUSR1
+	case "SIGUSR2":
+		sig = SIGUSR2
+	case "SIGHUP":
+		sig = syscall.SIGHUP
+	default:
+		fmt.Printf("invalid signal name %q\n", signalStr)
+		os.Exit(1)
+	}
+
+	name := formatFakeSignalEventNameWithPID(sig, pid)
+	if name == "" {
+		fmt.Printf("invalid signal name %q\n", signalStr)
+		os.Exit(1)
+	}
+
+	namep, err := windows.UTF16PtrFromString(name)
+	if err != nil {
+		fmt.Printf("failed to create utf16 string. %v\n", err)
+		os.Exit(1)
+	}
+
+	handle, err := windows.OpenEvent(windows.EVENT_MODIFY_STATE, false, namep)
+	if err != nil {
+		fmt.Printf("failed to create handle. %v\n", err)
+		fmt.Printf("the process with pid %d might not exist anymore\n", pid)
+		os.Exit(1)
+	}
+
+	err = windows.SetEvent(handle)
+	if err != nil {
+		fmt.Printf("failed to set event. %v", err)
+		os.Exit(1)
+	}
+}
